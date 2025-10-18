d24fff1d41108541491795335cff008dc1c4917e
Youssif Salama <221870241+YoussifSalama@users.noreply.github.com>
2025-10-19 01:10:40 +0300

    Update Home.tsx

--- app/components/pages/main/Home.tsx
+++ app/components/pages/main/Home.tsx
@@ -45,7 +45,7 @@ const Home = () => {
                  overflow-hidden"
     >
       <img
-        src="/youssif.jpg"
+        src="/youssif.jpeg"
         alt="Youssif Salama"
         className="rounded-full object-top object-cover w-[400px] h-[400px] 
                    max-md:w-[300px] max-md:h-[300px] 
